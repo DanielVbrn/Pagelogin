14baaa78aae71aa7c1817eed60172e6c8a23b390
daniel_vbrn <danielvbrn@gmail.com>
2022-08-30 10:04:21 -0300

    primeiro commit

---
+++ menu_interativo/menu.js
@@ -0,0 +1,20 @@
+function openMenu(){
+    document.getElementById("Mn_interactive").style.width = "100%"
+}
+function closeMenu(){
+    document.getElementById("Mn_interactive").style.width = "0%"
+    // alert("Retornando a página anterior!")
+}
+
+function openSobre(){
+    let curiosidades = document.getElementById("button-Sobre")
+    let SB = curiosidades.value
+
+    if(SB == addEventListener("click", openSobre)){
+        window.location.href = "https://www.creativosonline.org/pt/10-curiosidades-sobre-javascript.html"
+    }
+}
+
+function openServicos(){
+
+}
